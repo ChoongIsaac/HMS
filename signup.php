4334130ea7ed00a066f4d5fc0e905130c559ae08
ChongKahQing <205834@student.upm.edu.my>
2023-06-11 19:53:18 +0800

    solve security issue 2

--- signup.php
+++ signup.php
@@ -6,8 +6,9 @@
     <!-- meta tags -->
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="keywords" content="Art Sign Up Form Responsive Widget, Audio and Video players, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates,
-		Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
+    <meta name="keywords"
+        content="Art Sign Up Form Responsive Widget, Audio and Video players, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates,
+        Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
     <!-- /meta tags -->
     <!-- custom style sheet -->
     <link href="web/css/style.css" rel="stylesheet" type="text/css" />
@@ -16,9 +17,33 @@
     <link href="web/css/fontawesome-all.css" rel="stylesheet" />
     <!-- /fontawesome css -->
     <!-- google fonts-->
-    <link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
     <!-- /google fonts-->
 
+
+    <script>
+        function validatePassword() {
+            var password = document.getElementById("password").value;
+            var confirmPassword = document.getElementById("confirmPassword").value;
+
+            // Password should contain at least one lowercase letter, one uppercase letter, one digit, and one special character
+            var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+            if (!passwordRegex.test(password)) {
+                alert("Password must contain at least 8 characters, including at least one lowercase letter, one uppercase letter, one digit, and one special character.");
+                return false;
+            }
+
+            if (password !== confirmPassword) {
+                alert("Passwords do not match.");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
 </head>
 
 <?php
@@ -44,54 +69,73 @@ if (isset($_POST['signup-submit'])) {
         echo 'CSRF token not found in the request.<br>';
     }
 }
+
+if (isset($_POST['signup-submit'])) {
+    // Retrieve the password from the form submission
+    $password = $_POST['pwd'];
+
+    // Password should contain at least one lowercase letter, one uppercase letter, one digit, and one special character
+    $passwordRegex = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/';
+
+    if (!preg_match($passwordRegex, $password)) {
+        echo 'Password must contain at least 8 characters, including at least one lowercase letter, one uppercase letter, one digit, and one special character.';
+        exit;
+    }
+}
 ?>
 
 <body>
     <h1>Hostel Room Allocation System</h1>
     <div class=" w3l-login-form">
         <h2>Sign Up Here</h2>
-        <form action="includes/signup.inc.php" method="POST">
+        <form action="includes/signup.inc.php" method="POST" onsubmit="return validatePassword()">
             <input type="hidden" name="csrf_token" value=<?= htmlentities($token, ENT_QUOTES | ENT_HTML5, 'UTF-8') ?> />
             <div class=" w3l-form-group">
                 <label>Student Roll No</label>
                 <div class="group">
                     <i class="fas fa-id-badge"></i>
-                    <input type="text" class="form-control" name="student_roll_no" placeholder="Roll No" required="required" />
+                    <input type="text" class="form-control" name="student_roll_no" placeholder="Roll No"
+                        required="required" />
                 </div>
             </div>
             <div class=" w3l-form-group">
                 <label>First Name</label>
                 <div class="group">
                     <i class="fas fa-user"></i>
-                    <input type="text" class="form-control" name="student_fname" placeholder="First Name" required="required" />
+                    <input type="text" class="form-control" name="student_fname" placeholder="First Name"
+                        required="required" />
                 </div>
             </div>
             <div class=" w3l-form-group">
                 <label>Last Name</label>
                 <div class="group">
                     <i class="fas fa-user"></i>
-                    <input type="text" class="form-control" name="student_lname" placeholder="Last Name" required="required" />
+                    <input type="text" class="form-control" name="student_lname" placeholder="Last Name"
+                        required="required" />
                 </div>
             </div>
             <div class=" w3l-form-group">
                 <label>Mobile No</label>
                 <div class="group">
                     <i class="fas fa-phone"></i>
-                    <input type="text" class="form-control" name="mobile_no" placeholder="Mobile No" required="required" />
+                    <input type="text" class="form-control" name="mobile_no" placeholder="Mobile No"
+                        required="required" />
                 </div>
             </div>
             <div class=" w3l-form-group">
                 <label>Department</label>
                 <div class="group">
                     <i class="fas fa-graduation-cap"></i>
-                    <input type="text" class="form-control" name="department" placeholder="Department" required="required" />
+                    <input type="text" class="form-control" name="department" placeholder="Department"
+                        required="required" />
                 </div>
             </div>
             <div class=" w3l-form-group">
                 <label>Year of Study</label>
                 <div class="group">
                     <i class="fas fa-calendar"></i>
-                    <input type="text" class="form-control" name="year_of_study" placeholder="Year of Study" required="required" />
+                    <input type="text" class="form-control" name="year_of_study" placeholder="Year of Study"
+                        required="required" />
                 </div>
             </div>
 
@@ -107,7 +151,8 @@ if (isset($_POST['signup-submit'])) {
                 <label>Password:</label>
                 <div class="group">
                     <i class="fas fa-unlock"></i>
-                    <input type="password" class="form-control" name="pwd" placeholder="Password" required="required" />
+                    <input type="password" class="form-control" name="pwd" id="password" placeholder="Password"
+                        required="required" />
                 </div>
             </div>
 
@@ -115,7 +160,8 @@ if (isset($_POST['signup-submit'])) {
                 <label>Confirm Password:</label>
                 <div class="group">
                     <i class="fas fa-unlock"></i>
-                    <input type="password" class="form-control" name="confirmpwd" placeholder="Confirm Password" required="required" />
+                    <input type="password" class="form-control" name="confirmpwd" id="confirmPassword"
+                        placeholder="Confirm Password" required="required" />
                 </div>
             </div>
             <!--<div class="forgot">
@@ -127,7 +173,8 @@ if (isset($_POST['signup-submit'])) {
         <p class=" w3l-register-p">Already a member?<a href="index.php" class="register"> Login</a></p>
     </div>
     <footer>
-        <p class="copyright-agileinfo"> &copy; 2018 DBMS Project. All Rights Reserved | Design by <a href="https://www.linkedin.com/in/bharat-reddy/">Bharat-Prajwal-Rakesh</a></p>
+        <p class="copyright-agileinfo"> &copy; 2018 DBMS Project. All Rights Reserved | Design by <a
+                href="https://www.linkedin.com/in/bharat-reddy/">Bharat-Prajwal-Rakesh</a></p>
     </footer>
 
 </body>
