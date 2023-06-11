1a790702998f7081300c57ca5425cfaee6891415
Faizal Md Salleh <faizallmdsalleh@gmail.com>
2023-06-12 00:10:30 +0800

    Final update to implement automatic session timeout for improved security.

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,6 +1,29 @@
 <?php
   session_start();
-  $servername = "hms.local"; //change this  accordingly
+  
+  // Define the duration of inactivity after which the session should end
+  $inactive = 1800; // 30 minutes in seconds
+
+  // Check if the "timeout" variable has been set in the session
+  if (isset($_SESSION['timeout'])) {
+    // Calculate the session's "lifetime" by subtracting the current time from the "timeout"
+    $session_life = time() - $_SESSION['timeout'];
+	
+	// If the "lifetime" exceeds the defined period of inactivity, end the session
+	if ($session_life > $inactive) {
+		session_unset();
+		session_destroy();
+		header("Location: index.php"); // Redirect to login page
+		exit();
+	}	
+	
+  }
+
+  // Update the "timeout" with the current time
+  $_SESSION['timeout'] = time();
+  
+  
+  $servername = "hms.test"; //change this  accordingly
   $dBUsername = "root";
   $dBPassword = "";
   $dBName = "hostel_management_system";
